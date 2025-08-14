20bae864cb9b0fdb021f7cefebda7429e9977481
MicaheDev <webdev.michell@gmail.com>
2025-08-13 21:02:13 -0400

    fix(navigationBottomBar): useless things

--- src/components/NavigationBottomBar.tsx
+++ src/components/NavigationBottomBar.tsx
@@ -1,5 +1,4 @@
-import { Dumbbell, GraduationCap, House, UserPen } from "lucide-react";
-import { useEffect, useState } from "react";
+import { Dumbbell, GraduationCap, House, } from "lucide-react";
 import { NavLink } from "react-router";
 
 type NavigationBarProps = {
@@ -7,10 +6,13 @@ type NavigationBarProps = {
 };
 
 export default function NavigationBottomBar({ className }: NavigationBarProps) {
-  const [credentials, setCredentials] = useState<{
+/**
+ * 
+ *   const [credentials, setCredentials] = useState<{
     email: string;
     username: string;
   } | null>(null);
+
   const [error, setError] = useState<string | null>(null); // Para mostrar mensajes de error
 
   useEffect(() => {
@@ -35,6 +37,8 @@ export default function NavigationBottomBar({ className }: NavigationBarProps) {
 
     console.log(error);
   }, []);
+ */
+  
   return (
     <nav
       className={`h-[100px] bg-background dark:bg-dark-background  border-t border-characters w-full flex justify-center gap-6 items-center px-3 ${className}`}
